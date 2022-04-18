fd0b3141aacffaf7f05eb12c7e9bd2ab5bee8eb1
stephano1111 <stephanods@hotmail.com>
2022-04-18 16:24:53 -0500

    Vers2 de normalizar.py

--- normalizar.py
+++ normalizar.py
@@ -7,4 +7,20 @@ headers = ["preg","plas","pres","skin","insu","mass","pedi","age","class"]
 
 db = pd.read_csv(filename, names = headers)
 
-print(db.head())
+print("\n", db.head())
+
+print("\n Numero de instancias: ", len(db.axes[0]))
+
+"""
+print("\n", type(db["class"][0]), "\n")
+
+
+for i in range(len(db.axes[0])):
+    if db["class"][i] == "tested_positive":
+        db["class"][i] = 1
+    else: db["class"][i] = 0
+"""
+db.loc[db['class'] == 'tested_negative', 'class'] = '0'
+db.loc[db['class'] == 'tested_positive', 'class'] = '1'
+
+print("\n", db.head())
