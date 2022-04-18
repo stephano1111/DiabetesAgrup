7858b76cc1ff265d8b5d7da9b8e47b91a3ebbd21
stephano1111 <stephanods@hotmail.com>
2022-04-18 18:02:55 -0500

    Version usable del normalizar.py para la principal Agrup.py

--- normalizar.py
+++ normalizar.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 from sklearn import preprocessing
 
+"""
 filename = "Base/diabetes.csv"
 headers = ["preg","plas","pres","skin","insu","mass","pedi","age","class"]
 
@@ -16,7 +17,7 @@ db.loc[db['class'] == 'tested_negative', 'class'] = '0'
 db.loc[db['class'] == 'tested_positive', 'class'] = '1'
 
 print("\nDataFrame Class val 0/1: \n", db.head())
-
+"""
 #print("\n", headers[0])
 
 """
@@ -27,7 +28,21 @@ print("\n", arrayNorm[0])
 
 print("\n", len(arrayNorm[0]))
 """
-def normalizar(db):
+def normaliza():
+    filename = "Base/diabetes.csv"
+    headers = ["preg","plas","pres","skin","insu","mass","pedi","age","class"]
+
+    db = pd.read_csv(filename, names = headers)
+    pd.options.mode.chained_assignment = None
+
+    print("\nDataFrame Normal: \n", db.head())
+
+    print("\n Numero de instancias: ", len(db.axes[0]))
+
+    db.loc[db['class'] == 'tested_negative', 'class'] = '0'
+    db.loc[db['class'] == 'tested_positive', 'class'] = '1'
+
+    print("\nDataFrame Class val 0/1: \n", db.head())
     dbN = db
     for i in range(len(headers)-1):
         arrDim = np.array(db[headers[i]])
@@ -36,5 +51,7 @@ def normalizar(db):
             dbN[headers[i]][j] = arrayNorm[0][j]
     return dbN
 
-dbN = normalizar(db)
+"""
+dbN = normaliza(db)
 print("\n DataFrame Normalizado: \n", dbN.head())
+"""
