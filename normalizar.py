cf3c2f16acd1eb15abe790075406f5f5e552197c
stephano1111 <stephanods@hotmail.com>
2022-04-18 17:39:09 -0500

    Normalizar datos version funcional_1

--- normalizar.py
+++ normalizar.py
@@ -1,26 +1,40 @@
-from fileinput import filename
-from socket import herror
 import pandas as pd
+import numpy as np
+from sklearn import preprocessing
 
 filename = "Base/diabetes.csv"
 headers = ["preg","plas","pres","skin","insu","mass","pedi","age","class"]
 
 db = pd.read_csv(filename, names = headers)
+pd.options.mode.chained_assignment = None
 
-print("\n", db.head())
+print("\nDataFrame Normal: \n", db.head())
 
 print("\n Numero de instancias: ", len(db.axes[0]))
 
-"""
-print("\n", type(db["class"][0]), "\n")
-
-
-for i in range(len(db.axes[0])):
-    if db["class"][i] == "tested_positive":
-        db["class"][i] = 1
-    else: db["class"][i] = 0
-"""
 db.loc[db['class'] == 'tested_negative', 'class'] = '0'
 db.loc[db['class'] == 'tested_positive', 'class'] = '1'
 
-print("\n", db.head())
+print("\nDataFrame Class val 0/1: \n", db.head())
+
+#print("\n", headers[0])
+
+"""
+pregArr = np.array(db['preg'])
+print("\n", pregArr)
+arrayNorm = preprocessing.normalize([pregArr])
+print("\n", arrayNorm[0])
+
+print("\n", len(arrayNorm[0]))
+"""
+def normalizar(db):
+    dbN = db
+    for i in range(len(headers)-1):
+        arrDim = np.array(db[headers[i]])
+        arrayNorm = preprocessing.normalize([arrDim])
+        for j in range(len(arrayNorm[0])):
+            dbN[headers[i]][j] = arrayNorm[0][j]
+    return dbN
+
+dbN = normalizar(db)
+print("\n DataFrame Normalizado: \n", dbN.head())
