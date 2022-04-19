dcf19354d4d732c08bfe1ac56c1f499655ab2427
stephano1111 <stephanods@hotmail.com>
2022-04-18 20:12:12 -0500

    Actualiza versión3 no termindo

--- Agrup.py
+++ Agrup.py
@@ -1,7 +1,10 @@
 import normalizar
+import kms
 
 #Normalizar los datos del dataframe:
-dbN = normalizar.normaliza()
+#dbN = normalizar.normaliza()
+dbN = normalizar.minMaxScaler("preg","pres")
 print("\n DataFrame Normalizado: \n", dbN.head())
 #Fin de normalizar datos del dataframe.
 
+kms.kMENS(dbN,"preg","pres")
