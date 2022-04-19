b3b2ee348c12a4bb6f925487a830f8cd2390aea5
stephano1111 <stephanods@hotmail.com>
2022-04-18 20:14:38 -0500

    Version4 de normalizar.py modificación por problemas en kms con el header, no funciona

--- normalizar.py
+++ normalizar.py
@@ -1,14 +1,14 @@
 import pandas as pd
 import numpy as np
 from sklearn import preprocessing
+from sklearn.preprocessing import MinMaxScaler
 
-"""
 filename = "Base/diabetes.csv"
 headers = ["preg","plas","pres","skin","insu","mass","pedi","age","class"]
 
 db = pd.read_csv(filename, names = headers)
 pd.options.mode.chained_assignment = None
-
+"""
 print("\nDataFrame Normal: \n", db.head())
 
 print("\n Numero de instancias: ", len(db.axes[0]))
@@ -29,12 +29,12 @@ print("\n", arrayNorm[0])
 print("\n", len(arrayNorm[0]))
 """
 def normaliza():
-    filename = "Base/diabetes.csv"
-    headers = ["preg","plas","pres","skin","insu","mass","pedi","age","class"]
+    #filename = "Base/diabetes.csv"
+    #headers = ["preg","plas","pres","skin","insu","mass","pedi","age","class"]
 
-    db = pd.read_csv(filename, names = headers)
-    pd.options.mode.chained_assignment = None
-
+    #db = pd.read_csv(filename, names = headers)
+    #pd.options.mode.chained_assignment = None
+    
     print("\nDataFrame Normal: \n", db.head())
 
     print("\n Numero de instancias: ", len(db.axes[0]))
@@ -51,6 +51,18 @@ def normaliza():
             dbN[headers[i]][j] = arrayNorm[0][j]
     return dbN
 
+def minMaxScaler(a,b):
+    dbN2 = db
+    for i in headers:
+        if i != a:
+            if i != b:
+                dbN2 = dbN2.drop(i, axis=1)
+
+    min_max_scaler = MinMaxScaler() 
+    dbN2 = min_max_scaler.fit_transform(dbN2)
+    dbN2 = pd.DataFrame(dbN2) # Convertimos a Dataframe
+    
+    return dbN2
 """
 dbN = normaliza(db)
 print("\n DataFrame Normalizado: \n", dbN.head())
